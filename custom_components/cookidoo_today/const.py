bdcd99d39e0732e02cd924bef5e172c0fffaf31b
czajakamil <53714679+czajakamil@users.noreply.github.com>
2025-12-14 19:01:26 +0100

    Add platform definition for cookidoo_today

--- custom_components/cookidoo_today/const.py
+++ custom_components/cookidoo_today/const.py
@@ -1,5 +1,11 @@
+from homeassistant.const import Platform
+
 DOMAIN = "cookidoo_today"
+
 CONF_BASE_URL = "base_url"
 DEFAULT_BASE_URL = "http://127.0.0.1:8099"
+
 CONF_SCAN_INTERVAL = "scan_interval"
 DEFAULT_SCAN_INTERVAL = 15
+
+PLATFORMS: list[Platform] = [Platform.SENSOR]
