f1a966f555fb968e3d3083a60dd53d94b3976857
czajakamil <53714679+czajakamil@users.noreply.github.com>
2025-12-14 18:54:08 +0100

    Update constants for cookidoo_today component

--- custom_components/cookidoo_today/const.py
+++ custom_components/cookidoo_today/const.py
@@ -1,11 +1,5 @@
 DOMAIN = "cookidoo_today"
-
 CONF_BASE_URL = "base_url"
-CONF_EMAIL = "email"
-CONF_PASSWORD = "password"
-CONF_COUNTRY = "country"
-CONF_REFRESH_MINUTES = "refresh_minutes"
-
-DEFAULT_BASE_URL = "http://homeassistant.local:8099"
-DEFAULT_COUNTRY = "pl"
-DEFAULT_REFRESH_MINUTES = 15
+DEFAULT_BASE_URL = "http://127.0.0.1:8099"
+CONF_SCAN_INTERVAL = "scan_interval"
+DEFAULT_SCAN_INTERVAL = 15
