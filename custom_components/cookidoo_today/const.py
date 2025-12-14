5ae5646fe1336bbbd350652ce39f7f6ecbff1bd6
czajakamil <53714679+czajakamil@users.noreply.github.com>
2025-12-14 17:18:20 +0100

    Add base URL and default values to const.py

--- custom_components/cookidoo_today/const.py
+++ custom_components/cookidoo_today/const.py
@@ -1,9 +1,11 @@
 DOMAIN = "cookidoo_today"
 
+CONF_BASE_URL = "base_url"
 CONF_EMAIL = "email"
 CONF_PASSWORD = "password"
 CONF_COUNTRY = "country"
 CONF_REFRESH_MINUTES = "refresh_minutes"
 
+DEFAULT_BASE_URL = "http://homeassistant.local:8099"
 DEFAULT_COUNTRY = "pl"
 DEFAULT_REFRESH_MINUTES = 15
